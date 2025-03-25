b2a4a377f229341e0169fb7efc33b7f7e7d126e7
Samir Ahmane <55358999+Arka-cell@users.noreply.github.com>
2025-03-25 12:43:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    # Retrieve all environment variables as a dictionary
+    env_vars = dict(os.environ)
+    return jsonify(env_vars)
 
 @app.route('/how are you')
 def hello():
     return 'I am good, how about you?'
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=8001)
